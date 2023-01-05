43ae68740fb0e8804fb631975473183961ee35e3
alvaro <arecabarren14@alumnos.utalca.cl>
2023-01-05 07:49:27 -0300

    Add solution, all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let myArray = [];
+    for (const letter of string) {
+        myArray.splice(0,0,letter);
+    }
+    let myString = '';
+    for(const item of myArray){
+        if (item === ',') {continue;}
+        myString += item
+    } 
+    return myString
 };
 
 // Do not edit below this line
