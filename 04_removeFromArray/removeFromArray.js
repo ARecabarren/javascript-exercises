e3973990724889af1515a6f24d10fae12b643c39
alvaro <arecabarren14@alumnos.utalca.cl>
2023-01-08 08:54:24 -0300

    Pass 2 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,toRemove) {
+    for (let j = 1; j < arguments.length; j++) {
+         for (let i = 0; i < array.length; i++) {
+            if(arguments[j] === array[i]) array.splice(i,1);    
+        }     
+    }
+    return array
+   
 };
 
 // Do not edit below this line
