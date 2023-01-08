ecc6b007d021045dab574bfa32cd0d7ddeba3e7d
alvaro <arecabarren14@alumnos.utalca.cl>
2023-01-08 11:01:53 -0300

    Pass al tests of sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (typeof(a) == 'number' && typeof(b) == 'number') {
+        if(a > 0 && b > 0){
+            
+            let lower = Math.min(a,b);
+            let higher = Math.max(a,b);
+            let iterations = higher - lower;
+            let count = 0;
+            
+            for (let index = 1; index <= higher; index++) {
+                count += index;
+            
+            } return count
 
+        } else {return 'ERROR'}
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
