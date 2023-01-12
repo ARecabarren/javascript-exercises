114738cc59ef2773a816e88ad196cf87c7d45f2e
alvaro <arecabarren14@alumnos.utalca.cl>
2023-01-12 20:35:04 -0300

    Pass all calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(number1,number2) {
+	return number1 + number2
 };
 
-const subtract = function() {
-	
+const subtract = function(number1,number2) {
+	return number1 - number2
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	let counter = 0;
+  for(let item of myArray){
+    counter += item;
+  } return counter
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  let firstMultiplier = myArray[0];
+  for (let i = 1; i < myArray.length; i++) {
+    firstMultiplier *= myArray[i];
+    
+  }
+  return firstMultiplier
 };
 
-const power = function() {
-	
+const power = function(base,power) {
+	return base ** power
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0){
+    return 1
+  }else{
+    let base = num;
+    for(let i = base - 1; i > 1; i--){
+      base *= i
+    }
+    return base
+  }
 };
 
 // Do not edit below this line
