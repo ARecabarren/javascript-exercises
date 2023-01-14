76b67d85de77a4cbadf8f6de5d48e10cf03e8830
alvaro <arecabarren14@alumnos.utalca.cl>
2023-01-14 13:27:51 -0300

    Pass al test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+
+    let oldest;
+    let age = 0;
+    let today = new Date();
+    let tempAge;
+    
+    function computeAge(yearBirth) {
+        let age = (new Date()) - (new Date(`${yearBirth}`));
+        age = age / (1000*60*60*24*365)
+        return age
+    }
+    
+    for (const person of peopleArray) {
+        if (person.yearOfDeath == undefined) {
+            tempAge = computeAge(person.yearOfBirth)
+            if(tempAge > age){
+                age = tempAge; 
+                oldest = person;
+                continue
+            }
+            
+        }
+
+        tempAge = person.yearOfDeath - person.yearOfBirth
+        if( tempAge> age){age = tempAge; oldest = person }
+    }
+    return oldest
+
 
 };
 
