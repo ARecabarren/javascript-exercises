1e52a47bb15a4d064d81399ed73d5cce5e22d93c
alvaro <arecabarren14@alumnos.utalca.cl>
2023-01-14 10:23:30 -0300

    Pass test add html to debug

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(numTH) {
+    let penultimate = 0;
+    let last = 1;
+    let fiboNumber;
+    if(numTH<0){return 'OOPS'}
+    numTH = parseInt(numTH);
+    if(numTH == 1){return 1}
+    
+    for (let i = 0; i < numTH - 1; i++) {
+        fiboNumber = penultimate + last;
+        penultimate = last;
+        last = fiboNumber;
+    }
+    return fiboNumber    
 
 };
 
