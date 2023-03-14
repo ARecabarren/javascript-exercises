2e74e70e983325d2b8a5bccc292f7e7b5395984c
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-03-14 15:33:23 -0300

    Add sum and multiply using array methods

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,41 +1,71 @@
-const add = function(number1,number2) {
-	return number1 + number2
+// const add = function(number1,number2) {
+// 	return number1 + number2
+// };
+
+// const subtract = function(number1,number2) {
+// 	return number1 - number2
+// };
+
+// const sum = function(myArray) {
+// 	let counter = 0;
+//   for(let item of myArray){
+//     counter += item;
+//   } return counter
+// };
+
+// const multiply = function(myArray) {
+//   let firstMultiplier = myArray[0];
+//   for (let i = 1; i < myArray.length; i++) {
+//     firstMultiplier *= myArray[i];
+    
+//   }
+//   return firstMultiplier
+// };
+
+// const power = function(base,power) {
+// 	return base ** power
+// };
+
+// const factorial = function(num) {
+// 	if(num == 0){
+//     return 1
+//   }else{
+//     let base = num;
+//     for(let i = base - 1; i > 1; i--){
+//       base *= i
+//     }
+//     return base
+//   }
+// };
+
+// After array methods
+
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function(number1,number2) {
-	return number1 - number2
+const subtract = function(a,b) {
+	return a - b
 };
 
 const sum = function(myArray) {
-	let counter = 0;
-  for(let item of myArray){
-    counter += item;
-  } return counter
+	return myArray.reduce((total,numb) => {
+    return total + numb
+  },0)
 };
 
 const multiply = function(myArray) {
-  let firstMultiplier = myArray[0];
-  for (let i = 1; i < myArray.length; i++) {
-    firstMultiplier *= myArray[i];
-    
-  }
-  return firstMultiplier
+  return myArray.reduce((total,numb)=>{
+    return total * numb
+  },1)
 };
 
-const power = function(base,power) {
-	return base ** power
+const power = function() {
+	
 };
 
-const factorial = function(num) {
-	if(num == 0){
-    return 1
-  }else{
-    let base = num;
-    for(let i = base - 1; i > 1; i--){
-      base *= i
-    }
-    return base
-  }
+const factorial = function() {
+	
 };
 
 // Do not edit below this line
